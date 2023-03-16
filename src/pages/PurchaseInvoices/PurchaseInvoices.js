9f988409b6f9ae033d8287c6683f31b9142b6233
filizsgursan <filizzgursan@outlook.com>
2023-03-16 11:15:30 +0300

    code review

--- src/pages/PurchaseInvoices/PurchaseInvoices.js
+++ src/pages/PurchaseInvoices/PurchaseInvoices.js
@@ -3,9 +3,10 @@ import React from 'react'
 import { useState, useEffect, useLayoutEffect } from 'react'
 
 // React Native
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView ,TouchableWithoutFeedback,Keyboard} from 'react-native'
 import { VStack, HStack, ScrollView } from 'native-base'
 import { Icon } from 'react-native-elements'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 // Navigation
 import { useNavigation } from '@react-navigation/native'
@@ -94,8 +95,8 @@ export default function PurchaseInvoices() {
     }, [])
 
     useLayoutEffect(() => {
-        navigation.setOptions({   
-            title:"Onay Bekleyenler",        
+        navigation.setOptions({
+            title: "Onay Bekleyenler",
             headerSearchBarOptions: {
                 placeholder: "Search",
                 onChangeText: (event) => searchFilterFunction(event.nativeEvent.text)
@@ -177,83 +178,94 @@ export default function PurchaseInvoices() {
     return (
 
         <SafeAreaView style={{ flex: 1 }}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <KeyboardAwareScrollView
+                    extraHeight={0}
+                    style={{
+                        width: "100%",
+                        flex: 1
+                    }}
+                    contentContainerStyle={{ flex: 1 }}
+                >
 
-            <ScrollView style={styles.container} contentContainerStyle={{ paddingBottom: 100 }}>
+                    <ScrollView style={styles.container} contentContainerStyle={{ paddingBottom: 60 }}>
 
-                {
-                    data?.map((item, index) => {
-                        return (
-                            <HStack style={styles.list} key={index}>
-                                <HStack style={{ alignItems: "center", maxWidth: 324 }}>
-                                    <View
-                                        style={{
-                                            alignItems: 'center',
-                                            justifyContent: 'center',
-                                            backgroundColor: '#CCE2D9',
-                                            width: 24,
-                                            height: 24,
-                                            borderRadius: 12
-                                        }}
-                                    >
-                                        <Text
-                                            style={{
-                                                textAlign: 'center',
-                                                fontSize: 12,
-                                                lineHeight: 12,
-                                                color: '#007041'
-                                            }}
-                                        >
-                                            {item.number}
-                                        </Text>
-                                    </View>
-                                    <VStack style={{ paddingLeft: 8, maxWidth: 284 }} space={"5px"}>
-                                        <Text style={{ fontWeight: "bold", flexWrap: "wrap" }}>
-                                            {item.name}
-                                        </Text>
-
-                                        <Text style={{ fontSize: 15, color: "#6C6C6C" }}>
-                                            {item.date}
-                                        </Text>
-
-
-                                        <HStack style={{ justifyContent: "space-between" }}>
-                                            <HStack style={{ maxWidth: 156, paddingRight: 68 }}>
-                                                <Text style={{ flexWrap: "wrap", fontSize: 16, fontWeight: "bold" }}>
-                                                    Count
+                        {
+                            data?.map((item, index) => {
+                                return (
+                                    <HStack style={styles.list} key={index}>
+                                        <HStack style={{ alignItems: "center", maxWidth: 324 }}>
+                                            <View
+                                                style={{
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+                                                    backgroundColor: '#CCE2D9',
+                                                    width: 24,
+                                                    height: 24,
+                                                    borderRadius: 12
+                                                }}
+                                            >
+                                                <Text
+                                                    style={{
+                                                        textAlign: 'center',
+                                                        fontSize: 12,
+                                                        lineHeight: 12,
+                                                        color: '#007041'
+                                                    }}
+                                                >
+                                                    {item.number}
                                                 </Text>
-                                            </HStack>
-                                            <HStack style={{ maxWidth: 136 }}>
-                                                <Text style={{ flexWrap: "wrap", fontSize: 16, fontWeight: "bold" }}>
-                                                    {item.costTotal}
+                                            </View>
+                                            <VStack style={{ paddingLeft: 8, maxWidth: 284 }} space={"5px"}>
+                                                <Text style={{ fontWeight: "bold", flexWrap: "wrap" }}>
+                                                    {item.name}
                                                 </Text>
-                                            </HStack>
+
+                                                <Text style={{ fontSize: 15, color: "#6C6C6C" }}>
+                                                    {item.date}
+                                                </Text>
+
+
+                                                <HStack style={{ justifyContent: "space-between" }}>
+                                                    <HStack style={{ maxWidth: 156, paddingRight: 68 }}>
+                                                        <Text style={{ flexWrap: "wrap", fontSize: 16, fontWeight: "bold" }}>
+                                                            Count
+                                                        </Text>
+                                                    </HStack>
+                                                    <HStack style={{ maxWidth: 136 }}>
+                                                        <Text style={{ flexWrap: "wrap", fontSize: 16, fontWeight: "bold" }}>
+                                                            {item.costTotal}
+                                                        </Text>
+                                                    </HStack>
+                                                </HStack>
+                                            </VStack>
                                         </HStack>
-                                    </VStack>
-                                </HStack>
-                                <VStack>
-                                    <TouchableOpacity
-                                        hitSlop={{
-                                            top: 20,
-                                            bottom: 20,
-                                            left: 20,
-                                            right: 20,
-                                        }}
-                                        onPress={() => goToDetailScreen(item.name)}
-                                    >
-                                        <Icon
-                                            name="angle-right"
-                                            type="font-awesome"
-                                            size={20}
-                                            color="#A9A9A9"
-                                            style={{ marginRight: 16 }}
-                                        />
-                                    </TouchableOpacity>
-                                </VStack>                               
-                            </HStack>
-                        )
+                                        <VStack>
+                                            <TouchableOpacity
+                                                hitSlop={{
+                                                    top: 20,
+                                                    bottom: 20,
+                                                    left: 20,
+                                                    right: 20,
+                                                }}
+                                                onPress={() => goToDetailScreen(item.name)}
+                                            >
+                                                <Icon
+                                                    name="angle-right"
+                                                    type="font-awesome"
+                                                    size={20}
+                                                    color="#A9A9A9"
+                                                    style={{ marginRight: 16 }}
+                                                />
+                                            </TouchableOpacity>
+                                        </VStack>
+                                    </HStack>
+                                )
 
-                    })}
-            </ScrollView>
+                            })}
+                    </ScrollView>
+                </KeyboardAwareScrollView>
+            </TouchableWithoutFeedback>
         </SafeAreaView>
 
     )
